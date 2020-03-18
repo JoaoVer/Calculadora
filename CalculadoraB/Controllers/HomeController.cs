6bad11c6ceae641bfa2acfbb62d85ca95b77a0b9
JoaoVerissimo <joao17andre@hotmail.com>
2020-03-18 16:31:21 +0000

    View + receber dados

--- CalculadoraB/Controllers/HomeController.cs
+++ CalculadoraB/Controllers/HomeController.cs
@@ -24,8 +24,28 @@ namespace CalculadoraB.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(String visor)
+        public IActionResult Index(String visor, string bt)
         {
+            switch(bt){
+
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    if (visor == "0") visor = bt;
+                    else visor += bt; //visor = visor + bt
+                        break;
+
+            }
+
+            //enviar dados do Visor para a View
+            //acabar esta parte
             return View();
         }
 
